4c8f798a21447437f677b73c3ce6601bf3db146f
saipavan4572 <saipavan4572@gmail.com>
2024-12-31 12:32:10 +0530

    update ecr

--- 80-ecr/main.tf
+++ 80-ecr/main.tf
@@ -1,6 +1,9 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.project_name}-backend"
   image_tag_mutability = "IMMUTABLE"
+  ## possible vaules: MUTABEL ; IMMUTABLE   --- Default --> MUTABLE
+  ## IMMUTABLE - When ever we update the tag then it will create new image
+  ## MUTABLE   - When ever we update the tag then it will update/replace the existing image.
 
   image_scanning_configuration {
     scan_on_push = true
